38fbedf4681c112fd20f2cd9ec495325405169d6
_Navire_ <132008539+verifiedNavire@users.noreply.github.com>
2023-12-11 15:40:45 +0100

    Update cytruck.sh

--- cytruck.sh
+++ cytruck.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+n=$*
+m=$#
+l=$3
+for i in $*
+do
+	if [ '-h' == $i ]
+	then
+		echo "Vous ne pouvez pas introduire un argument qui vaut -h"
+		exit
+	fi
+done
 fichier=`cat data.csv | head -10 | tail -9 | cut -d';' -f6| sort -r -d`
+echo $n
+echo $m
+echo $l
 echo $fichier
